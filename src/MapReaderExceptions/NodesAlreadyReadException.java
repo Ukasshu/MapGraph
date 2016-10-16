4f8bdceee3aca11096867b60d19bdc8d9e530dc2
Łukasz Mielczarek <mielczareklukasz96@gmail.com>
2016-10-16 14:20:52 +0200

    Utworzenie wyjątku NodesAlreadyReadException

---
+++ src/MapReaderExceptions/NodesAlreadyReadException.java
@@ -0,0 +1,10 @@
+package MapReaderExceptions;
+
+/**
+ * Created by lukasz on 16.10.16.
+ */
+public class NodesAlreadyReadException extends Exception{
+    public NodesAlreadyReadException(String s){
+        super(s);
+    }
+}
