91c7f7b1a09f36af1414e8d5d6595ce36a2d0e48
Łukasz Mielczarek <mielczareklukasz96@gmail.com>
2016-10-23 12:59:11 +0200

    Dodany JavaDoc do DataConverter

--- src/DataConverter.java
+++ src/DataConverter.java
@@ -1,79 +1,130 @@
-import MapElements.*;
-import DataConverterExceptions.*;
+import DataConverterExceptions.DataNotConvertedYetException;
+import MapElements.Node;
+import MapElements.Way;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 /**
- * Created by lukasz on 18.10.16.
+ * Class providing data converting
+ *
+ * @author Łukasz Mielczarek
+ * @version 21.10.2016
  */
 public class DataConverter {
+    /**
+     * Holds Nodes provided by MapReader that are changed then
+     */
     private HashMap<String, Node> nodes;
+    /**
+     * Holds Ways provided by MapReader
+     */
     private ArrayList<Way> ways;
-    private boolean areDataConververted = false;
+    /**
+     * Remembers if the data have been converted
+     */
+    private boolean areDataConverted;
+    /**
+     * Holds Nodes that will be from HashMap
+     */
+    private ArrayList<Node> unnecessaryNodes;
 
+    /**
+     * DataConverter's constructor
+     * @param nodes HashMap with Nodes read from file
+     * @param ways ArrayList with Ways read from file
+     */
     public DataConverter(HashMap<String, Node> nodes, ArrayList<Way> ways){
         this.nodes = nodes;
         this.ways = ways;
+        this.unnecessaryNodes = new ArrayList<>();
+        this.areDataConverted = false;
     }
 
-    private void removeUnnecessaryNodes() {
-        ArrayList<Node> unnecessaryNodes = new ArrayList<>();
-        for(String key: nodes.keySet()){
-            if(!nodes.get(key).isNeeded()){
-                unnecessaryNodes.add(nodes.get(key));
+    /**
+     * Marks Nodes that will be removed
+     */
+    private void markUnnecessaryNodes(){
+        Node currentNode;
+        ListIterator<Node> iterator;
+        for(Way way: ways){
+            iterator = way.getNodes().listIterator(0);
+            if(!way.isRoundabout()){
+                currentNode = iterator.next();
+                currentNode.setNeeded();
+            }
+            currentNode = iterator.next();
+            while(iterator.hasNext()){
+                if(currentNode.getWaysCounter() <=1){
+                    iterator.remove();
+                    unnecessaryNodes.add(currentNode);
+                }
+                else{
+                    currentNode.setNeeded();
+                }
+                currentNode = iterator.next();
+            }
+            if(!way.isRoundabout()){
+                currentNode.setNeeded();
+            }
+            else if(currentNode.getWaysCounter()<=1){
+                iterator.remove();
             }
         }
-        for(Node node : unnecessaryNodes){
+    }
+
+    /**
+     * Removes unnecessary Nodes
+     */
+    private void removeUnnecessaryNodes(){
+        for(Node node: nodes.values()){
+            if(!node.isNeeded()){
+                unnecessaryNodes.add(node);
+            }
+        }
+        for(Node node: unnecessaryNodes){
             nodes.remove(node.getId());
         }
     }
 
-    private void markUnnecessaryNodes(){
-        Node currentNode;
-        for(Way way: ways){
-            if(!way.isRoundabout()){
-                way.getNodes().get(0).setNeeded();
-                way.getNodes().get(way.getNodes().size()-1).setNeeded();
-            }
-            Iterator<Node> iterator = way.getNodes().listIterator(0);
-
-            while(iterator.hasNext()){
-                currentNode = iterator.next();
-                if(currentNode.getWaysCounter() > 1){
-                    currentNode.setNeeded();
-                }
-            }
-        }
-    }
-
+    /**
+     * Adds connections to the Nodes that are connected on map
+     */
     private void convertWaysIntoEdges(){
-        Node previousNode;
-        //int nodesCounter = 1;
-        for(Way way:ways){
-            previousNode = null;
-            for(Node node: way.getNodes()){
-                if(node.isNeeded()) {
-                    if (previousNode != null && Long.parseLong(node.getId()) < Long.parseLong(previousNode.getId())) {
-                        node.addEdge(previousNode);
-                        previousNode.addEdge(node);
-                    }
-                    previousNode = node;
+        ListIterator<Node> iterator;
+        Node previousNode, currentNode;
+        for(Way way: ways){
+            iterator = way.getNodes().listIterator(0);
+            if(iterator.hasNext()) {
+                currentNode = iterator.next();
+                while (iterator.hasNext()) {
+                    previousNode = currentNode;
+                    currentNode = iterator.next();
+                    previousNode.addEdge(currentNode);
+                    currentNode.addEdge(previousNode);
                 }
             }
         }
     }
 
+    /**
+     * Calls all methods needed to convert data
+     */
     public void runConverter(){
         this.markUnnecessaryNodes();
         this.removeUnnecessaryNodes();
         this.convertWaysIntoEdges();
-        this.areDataConververted = true;
+        this.areDataConverted = true;
     }
 
-    public HashMap<String, Node> returnConvertedNodes() throws DataNotConvertedYetException {
-        if(!areDataConververted){
+    /**
+     * Returns container with converted data
+     * @return
+     * @throws DataNotConvertedYetException
+     */
+    public HashMap<String, Node> returnConvertedNodes() throws DataNotConvertedYetException{
+        if(!areDataConverted){
             throw new DataNotConvertedYetException("There was an attempt to return data without converting them!");
         }
         return nodes;
